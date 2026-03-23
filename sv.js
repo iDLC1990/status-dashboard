a67cd167939cff87b8ca7dca4056f953eb81abc1
iDLC1990 <ulanwork1990@gmail.com>
2026-03-23 09:41:39 +0100

    Update sv.js

--- sv.js
+++ sv.js
@@ -1,8 +1,22 @@
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+const CACHE_NAME = 'lcdr-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Установка воркера и кеширование ресурсов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    // Просто пропускает запросы через себя
-    event.respondWith(fetch(event.request));
+// Ответы на запросы
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
